6bdf2bd348b81093849b09905a347fd0da4f303b
Ruan Portella <ruanmorales29@gmail.com>
2024-10-26 22:48:34 -0300

    Feat: Cria Tabela Dinamica

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+import { columns } from './columns';
+import { DataTable } from '@/components/data-table';
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts';
+
+export default function Accounts() {
+  const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  return (
+    <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+      <Card className='border-none drop-shadow-sm'>
+        <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
+          <CardTitle className='text-xl line-clamp-1'>Accounts</CardTitle>
+          <Button onClick={newAccount.onOpen} size='sm'>
+            <Plus className='size-4 mr-2' />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={accounts} filterKey='email' onDelete={() => {}} disabled={false} />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
