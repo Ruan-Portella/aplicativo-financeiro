58788636324e24acd39df0260ce489a604acd814
Ruan Portella <ruanmorales29@gmail.com>
2024-11-09 22:01:42 -0300

    Feat: Tenta Arrumar error de cors

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,20 +4,10 @@ import accounts from "./accounts";
 import categories from "./categories";
 import transactions  from "./transactions";
 import summary from "./summary";
-const cors = require('cors');
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
-
-const corsOptions = {
-    origin: process.env.NEXT_PUBLIC_API_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
-  };
-  
-
-app.use(cors(corsOptions))
 
 const routes = app
     .route('/accounts', accounts)
